cd548784e2e184243f8759b85e87644049a6b58c
Janek <eunho.moon@gmail.com>
2022-12-26 23:16:50 +0900

    Entity Mapping

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,8 +10,16 @@ public class Member {
 
     @Id
     private Long id;
-
+    @Column(unique = true, length = 10)
     private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
