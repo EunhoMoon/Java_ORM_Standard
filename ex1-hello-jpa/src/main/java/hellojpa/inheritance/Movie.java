0b7b95e401e111531508c6f0e99d608794814dec
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-01-03 11:58:35 +0900

    상속 매핑

---
+++ ex1-hello-jpa/src/main/java/hellojpa/inheritance/Movie.java
@@ -0,0 +1,40 @@
+package hellojpa.inheritance;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+//@DiscriminatorValue("M")
+public class Movie extends Item {
+
+    private String director;
+    private String actor;
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "director='" + director + '\'' +
+                ", actor='" + actor + '\'' +
+                ", id='" + this.getId() + '\'' +
+                ", name='" + this.getName() + '\'' +
+                ", price='" + this.getPrice() + '\'' +
+                '}';
+    }
+
+}
