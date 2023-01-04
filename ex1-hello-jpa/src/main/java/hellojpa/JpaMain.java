002e41e7e454a715da0b9969421aef370f2b7f3d
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-01-04 14:54:11 +0900

    프록시

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,8 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.List;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 
 public class JpaMain {
 
@@ -15,64 +12,20 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
-
         try {
-            // 등록
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("helloB");
+            Member member = new Member();
+            member.setName("memberA");
+            member.setAge(20);
+            em.persist(member);
 
-//            em.persist(member);
+            em.flush();
+            em.clear();
 
-            // 조회
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
+            Member findMember = em.getReference(Member.class, member.getId());
+            System.out.println(findMember instanceof Member);
+            Hibernate.initialize(findMember);
 
-            // 수정
-//            findMember.setName("HelloJPA"); // 트랜잭션이 커밋되기 전에 변경된 값이 있는지 체크한 후 Update
-
-            // 삭제
-//            em.remove(findMember);
-
-            // JPQL
-            List<Member> result = em.createQuery("SELECT m FROM Member AS m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-//            for (Member member : result) {
-//                System.out.println("member.name = " + member.getName());
-//            }
-            Team team = new Team();
-            team.setName("TeamA");
-            team.setCreatedDate(LocalDateTime.now());
-            em.persist(team);
-
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setCreatedDate(LocalDateTime.now());
-            member1.changeTeam(team);
-
-            Member member2 = new Member();
-            member2.setCreatedDate(LocalDateTime.now());
-            member2.setName("member2");
-
-            member2.changeTeam(team);
-            em.persist(member1);
-            em.persist(member2);
-
-//            em.flush();
-//            em.clear();
-//            team.getMembers().add(member1);
-//            team.getMembers().add(member2);
-
-            Member findMember = em.find(Member.class, member1.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-            for (Member m : members) {
-                System.out.println("member=" + m.getName());
-            }
-
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(findMember));
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
