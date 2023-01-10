8e11dbe72b2effd962e227746e53ca2cda2c0300
Janek <eunho.moon@gmail.com>
2023-01-10 23:56:06 +0900

    값 타입 컬렉션

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,7 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -22,10 +23,24 @@ public class JpaMain {
             member.getFavoriteFoods().add("피자");
             member.getFavoriteFoods().add("삽겹살");
 
-            member.getAddressHistory().add(new Address("old1", "street", "12345"));
-            member.getAddressHistory().add(new Address("old2", "street", "12345"));
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "12345"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "12345"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+//            List<Address> addressHistory = findMember.getAddressHistory();
+//            Address oldAddr = findMember.getHomeAddress();
+//            findMember.setHomeAddress(new Address("new city", "street", "12345"));
+//            addressHistory.add(oldAddr);
+//            addressHistory.remove(new Address("old1", "street", "12345"));
+//            addressHistory.add(new Address("old city", "street", "12345"));
+//
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("비빔밥");
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -30,9 +30,9 @@ public class Member extends BaseEntity {
     @Column(name = "FOOD_NAME")
     private Set<String> favoriteFoods = new HashSet<>();
 
-    @ElementCollection
-    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
-    private List<Address> addressHistory = new ArrayList<>();
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -66,11 +66,11 @@ public class Member extends BaseEntity {
         this.favoriteFoods = favoriteFoods;
     }
 
-    public List<Address> getAddressHistory() {
+    public List<AddressEntity> getAddressHistory() {
         return addressHistory;
     }
 
-    public void setAddressHistory(List<Address> addressHistory) {
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
         this.addressHistory = addressHistory;
     }
 }
